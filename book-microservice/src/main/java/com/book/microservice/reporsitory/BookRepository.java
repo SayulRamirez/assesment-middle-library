d2fd16cf247c03ecfeb6bbc0f68cd987861ddcc1
SayulRamirez <sayulramirez@gmail.com>
2025-07-20 17:48:53 -0600

    feature: se agrego el repositorio para los libros

---
+++ book-microservice/src/main/java/com/book/microservice/reporsitory/BookRepository.java
@@ -0,0 +1,10 @@
+package com.book.microservice.reporsitory;
+
+import com.book.microservice.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
