4d1defbeb56ac116943ab1cf728e5b5261066694
SayulRamirez <sayulramirez@gmail.com>
2025-07-24 21:25:53 -0600

    feature: se agrego y configuro el microservicio gateway

---
+++ gateway-library/src/main/java/com/gateway/library/GatewayLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayLibraryApplication.class, args);
+	}
+
+}
---
+++ gateway-library/src/test/java/com/gateway/library/GatewayLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
