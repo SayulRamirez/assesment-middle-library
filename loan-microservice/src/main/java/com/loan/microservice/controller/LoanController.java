879e1689cd2a2dd8c9bb2a05228145e879cda470
SayulRamirez <sayulramirez@gmail.com>
2025-07-24 19:11:02 -0600

    feature: se agrego el controlador

---
+++ loan-microservice/src/main/java/com/loan/microservice/controller/LoanController.java
@@ -0,0 +1,35 @@
+package com.loan.microservice.controller;
+
+import com.loan.microservice.dto.LoanRequest;
+import com.loan.microservice.dto.LoanResponse;
+import com.loan.microservice.service.LoanService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/loan")
+@RequiredArgsConstructor
+public class LoanController {
+
+    private final LoanService service;
+
+    @PostMapping
+    public ResponseEntity<LoanResponse> addLoan(@Valid @RequestBody LoanRequest request) {
+        return ResponseEntity.ok(service.addLoan(request.idBook(), request.idUser()));
+    }
+
+    @PutMapping("{idLoan}")
+    public ResponseEntity<Void> toTurnIn(@PathVariable Integer idLoan) {
+        service.toTurnIn(idLoan);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("{idUser}")
+    public ResponseEntity<List<LoanResponse>> findAllByIdUser(@PathVariable Integer idUser) {
+        return ResponseEntity.ok(service.findLoansByIdUser(idUser));
+    }
+}
