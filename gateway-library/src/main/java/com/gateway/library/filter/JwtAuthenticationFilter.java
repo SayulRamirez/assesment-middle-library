042349f089f09548c5dd55123c44fa83ea02be46
SayulRamirez <sayulramirez@gmail.com>
2025-07-28 21:41:02 -0600

    feature: se agrego el filtro para la autenticacion

---
+++ gateway-library/src/main/java/com/gateway/library/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,107 @@
+package com.gateway.library.filter;
+
+import com.gateway.library.util.JwtUtil;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements WebFilter {
+
+    private final JwtUtil jwtUtil;
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        System.out.println(path);
+        if (path.startsWith("/auth")) {
+            return chain.filter(exchange);
+        }
+
+        String token = getTokenFromRequest(exchange);
+
+        System.out.println(token);
+        if (token == null) return unauthorized(exchange);
+
+        String username = jwtUtil.getUsernameFromToken(token);
+
+//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+//        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+//        if (!jwtUtil.isTokenValid(token, userDetails)) {
+//            return unauthorized(exchange);
+//        }
+
+//        Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(username, null, List.of());
+        return chain.filter(exchange)
+                        .contextWrite(ReactiveSecurityContextHolder
+                                .withSecurityContext(Mono.just(new SecurityContextImpl(authToken))));
+
+//        }
+
+
+        /*String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        System.out.println("Auth Header: " + authHeader);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return unauthorized(exchange);
+        }
+
+        return chain.filter(exchange)
+                .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(new SecurityContextImpl(a))))
+*/
+//        String token = authHeader.substring(7);
+//
+//        System.out.println("Token: " + token);
+//        if (!jwtUtil.validateToken(token)) {
+//            return unauthorized(exchange);
+//        }
+//
+//        String username = jwtUtil.extractUsername(token);
+//
+//        Authentication auth = new UsernamePasswordAuthenticationToken(username, null, List.of());
+//
+//        return chain.filter(exchange)
+//                .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(new SecurityContextImpl(auth))));
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    private String getTokenFromRequest(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+
+        return null;
+    }
+}
