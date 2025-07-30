00216aab24861cd6b47ed085062fbe4615460b3c
SayulRamirez <sayulramirez@gmail.com>
2025-07-29 19:48:47 -0600

    config: se agrego la configuracion de la seguridad

--- gateway-library/src/main/java/com/gateway/library/config/SecurityConfig.java
+++ gateway-library/src/main/java/com/gateway/library/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import com.gateway.library.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -18,17 +16,14 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-//    private final AuthenticationProvider authenticationProvider;
-
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/auth/**", "/api/v1/book/all").permitAll()
                         .anyExchange().authenticated()
                 )
-//                .authenticationManager((ReactiveAuthenticationManager) authenticationProvider)
                 .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
     }
