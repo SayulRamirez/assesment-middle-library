414bb8051ecaed6d94849dadc5bc27ee7aff6705
SayulRamirez <sayulramirez@gmail.com>
2025-07-29 19:50:43 -0600

    feature: se agrego el controlador de autenticacion con los endpoints de login y registro

--- gateway-library/src/main/java/com/gateway/library/controller/AuthController.java
+++ gateway-library/src/main/java/com/gateway/library/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.gateway.library.dao.AuthResponse;
 import com.gateway.library.dao.LoginRequest;
 import com.gateway.library.dao.RegisterRequest;
 import com.gateway.library.service.AuthService;
-import com.gateway.library.util.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,25 +22,18 @@ public class AuthController {
 
     private final AuthService authService;
 
-    private final JwtUtil jwtUtil;
-
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
 
-        if ("admin".equals(request.username()) && "password".equals(request.password())) {
-            String token = jwtUtil.generateToken(request.username());
+        Optional<AuthResponse> login = authService.login(request);
 
-            return ResponseEntity.ok(Collections.singletonMap("token", token));
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        return ResponseEntity.ok(authService.login(request));
+        return login.isPresent() ?
+                ResponseEntity.ok(login.get()) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody RegisterRequest request) {
         authService.register(request);
-
         return ResponseEntity.ok().build();
     }
 }
